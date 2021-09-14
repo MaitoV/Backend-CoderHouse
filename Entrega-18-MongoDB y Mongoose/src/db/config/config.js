30a30e4e84e3c0c7b7be1f82bd91acff7b1ae6ed
Maito <maite.v@outlook.com>
2021-09-13 23:04:48 -0300

    migracion de las operaciones de productos y mensajes a mongoose

--- Entrega-18-MongoDB y Mongoose/src/db/config/config.js
+++ Entrega-18-MongoDB y Mongoose/src/db/config/config.js
@@ -1,6 +1,4 @@
-const knex = require('knex');
 const mongoose = require('mongoose');
-const initProducts = require('../seeds/products');
 const URL = 'mongodb://localhost:27017/ecommerce';
 
 const mongoDB = async () => {
