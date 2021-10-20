5222064456f9098e28e95f3352e23fa5ecb478ff
Maito <maite.v@outlook.com>
2021-10-19 23:52:44 -0300

    entrega 26 Cluster

---
+++ Entrega-26-Cluster/src/db/models/usersChat.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const usersCollection = 'usersChat';
+
+const usersSchema = new mongoose.Schema({
+    socket_id: {type: String, required: true},
+    email: {type: String, required: true},
+    avatar: {type: String, required: true}
+}, {versionKey: false});
+
+const users = new mongoose.model(usersCollection, usersSchema);
+
+module.exports = users;
